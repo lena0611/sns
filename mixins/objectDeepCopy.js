b0c9afb79f23aca02362578357438b68e88cabf9
lena0611 <lena0611@gmail.com>
2019-05-21 20:03:35 +0900

    객체 깊은복사  믹스인 메소드 추가

---
+++ mixins/objectDeepCopy.js
@@ -0,0 +1,15 @@
+export default {
+  methods: {
+    objectDeepCopy(obj) {
+      let clone = {}
+      for (let i in obj) {
+        if (typeof obj[i] == 'object' && obj[i] != null) {
+          clone[i] = cloneObject(obj[i])
+        } else {
+          clone[i] = obj[i]
+        }
+      }
+      return clone
+    }
+  }
+}
