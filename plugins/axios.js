d1ec3f2cb466f5032177d4838fd955f4a264577b
lena0611 <lena0611@gmail.com>
2019-05-21 20:00:34 +0900

    axios 요청시 lang정보 해더추가

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
 export default ({ $axios, store }) => {
   $axios.onRequest(config => {
+    config.headers.lang = store.state.lang
     if (store.state.user.token) {
       config.headers.Authorization = `Bearer ${store.state.user.token}`
     }
